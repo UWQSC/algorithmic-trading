8fd4cc04a4741e4987c4bb42481a7e4e808f24ec
TheFJcurve <sargunbhatti27@gmail.com>
2025-02-03 15:46:13 -0500

    Added sample interface and implementation
    topic/#9

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -4,6 +4,9 @@ Sample test file
 
 import unittest
 
+from interfaces.sample_interface import ISampleClass
+from src.sample_impl import SampleClassImpl
+
 
 class SampleTest(unittest.TestCase):
     """
@@ -14,11 +17,13 @@ class SampleTest(unittest.TestCase):
         """
         This function runs before every unit test.
         """
+        pass
 
     def tearDown(self):
         """
         This function runs after every unit test.
         """
+        pass
 
     def test_sample(self):
         """
@@ -26,3 +31,22 @@ class SampleTest(unittest.TestCase):
         """
 
         self.assertEqual(2 + 2, 4)
+
+    def test_sample_interface_raises_exception(self):
+        """
+        Testing our sample implementation
+        """
+        
+        def invalid_function():
+            interface_class: ISampleClass = ISampleClass()
+        
+        self.assertRaises(TypeError, invalid_function)
+
+    def test_sample_implementation_does_not_raise_exception(self):
+        """
+        Testing our sample implementation
+        """
+        
+        interface_class: ISampleClass = SampleClassImpl()
+        self.assertEqual(interface_class.say_hi_to_mom(), "Hi Mom")
+        
