d7fb9b72697263889cf5449cbec4db4bc61bc370
kmkan <kandpalkbusiness@gmail.com>
2025-03-09 16:07:13 -0400

    Complete unit tests for missing values function for HMM pre-processor
    topic/#78

--- uwqsc_algorithmic_trading/tests/preprocessing/hmm_preprocessor_impl_test.py
+++ uwqsc_algorithmic_trading/tests/preprocessing/hmm_preprocessor_impl_test.py
@@ -9,3 +9,15 @@ class HMMPreprocessorImplTest(unittest.TestCase):
     """
     This class is used to test each component of Hidden Markov Model's preprocessor
     """
+
+    def test_missing_values_extrapolates_na_values(self):
+        """
+        Testing that missing values are correctly handled
+        """
+        
+        self.preprocessor.load_data()
+        self.preprocessor.__processed_data__.loc[self.start_date] = [None for _ in self.tickers]
+        self.preprocessor.missing_values()
+        is_na_in_data = self.preprocessor.__processed_data__.isna().any()
+        is_na_in_data = is_na_in_data.to_list()
+        self.assertFalse(is_na_in_data[0])
