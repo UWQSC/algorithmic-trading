29316f10446373ff130d7cb1040476687fe100a0
TheFJcurve <sargunbhatti27@gmail.com>
2025-03-30 17:03:20 -0400

    setting up the tensorflow models
    topic/#88

--- uwqsc_algorithmic_trading/src/algorithms/simple_tensorflow_impl.py
+++ uwqsc_algorithmic_trading/src/algorithms/simple_tensorflow_impl.py
@@ -1,5 +1,5 @@
 """
-
+This file sets up logic for a tensorflow based algorithmic trading algorithm.
 """
 
 from typing import List, Dict, Any, Optional
@@ -16,7 +16,9 @@ from uwqsc_algorithmic_trading.src.preprocessing.st_preprocessor_impl import STP
 
 class SimpleTensorFlowImpl(IAlgorithm):
     """
-    
+    Sets up a simple tensorflow model.
+    This implementation will be trained on pre-existing algorithms.
+    No logic is included, as we wish to see comparison with a base level tensorflow model.
     """
 
     def __init__(self,
@@ -26,35 +28,73 @@ class SimpleTensorFlowImpl(IAlgorithm):
         processor = STPreProcessorImpl()
         super().__init__(name, tickers, processor, parameters)
 
-        self.__model__: Optional[Sequential] = None
-        self.define_model()    
+        self.__generate_signals_model__: Optional[Sequential] = None
+        self.__calculate_position_size_model__: Optional[Sequential] = None
+        self.define_model()
 
     def define_model(self):
         """
-        
+        Define and set up the tensorflow models that will predict the outputs of core functions.
+        We define two models
+
+        1. __generate_signals_model__:
+        Input shape is the size of all tickers.
+        Output is processed with tanh function (between -1 to 1) to generate Stock Positions.
+
+        2. __calculate_position_size_model__:
+        Input shape is the size of all tickers.
+        Output is processed with linear function (-infinity to infinity) to generate Position Size.
         """
 
-        self.__model__ = Sequential()
+        self.__generate_signals_model__ = Sequential()
+        self.__calculate_position_size_model__ = Sequential()
 
-        self.__model__.add(Input(shape=(len(self.tickers),)))
-        self.__model__.add(Dense(128, activation='relu'))
-        self.__model__.add(Dropout(0.5))
-        self.__model__.add(Dense(units=256))
-        self.__model__.add(Dropout(0.5))
-        self.__model__.add(Dense(units=128))
-        self.__model__.add(Dropout(0.5))
-        self.__model__.add(Dense(units=32))
-        self.__model__.add(Dropout(0.5))
-        self.__model__.add(Dense(units=len(self.tickers)))
+        self.__generate_signals_model__.add(Input(shape=(len(self.tickers),)))
+        self.__generate_signals_model__.add(Dense(1024, activation='tanh'))
+        self.__generate_signals_model__.add(Dropout(0.5))
+        self.__generate_signals_model__.add(Dense(units=2048))
+        self.__generate_signals_model__.add(Dropout(0.5))
+        self.__generate_signals_model__.add(Dense(units=1024))
+        self.__generate_signals_model__.add(Dropout(0.5))
+        self.__generate_signals_model__.add(Dense(units=512))
+        self.__generate_signals_model__.add(Dropout(0.5))
+        self.__generate_signals_model__.add(Dense(units=len(self.tickers)))
 
-        self.__model__.compile(
+        self.__calculate_position_size_model__.add(Input(shape=(len(self.tickers),)))
+        self.__calculate_position_size_model__.add(Dense(1024, activation='linear'))
+        self.__calculate_position_size_model__.add(Dropout(0.5))
+        self.__calculate_position_size_model__.add(Dense(units=2048))
+        self.__calculate_position_size_model__.add(Dropout(0.5))
+        self.__calculate_position_size_model__.add(Dense(units=1024))
+        self.__calculate_position_size_model__.add(Dropout(0.5))
+        self.__calculate_position_size_model__.add(Dense(units=512))
+        self.__calculate_position_size_model__.add(Dropout(0.5))
+        self.__calculate_position_size_model__.add(Dense(units=len(self.tickers)))
+
+        self.__generate_signals_model__.compile(
             loss='mean_squared_error',
-            optimizer=Adam(learning_rate=0.001),
+            optimizer=Adam(),
+            metrics=['accuracy']
+        )
+
+        self.__calculate_position_size_model__.compile(
+            loss='mean_squared_error',
+            optimizer=Adam(),
             metrics=['accuracy']
         )
 
     def generate_signals(self, current_data: pd.DataFrame):
-        pass
+        self.__generate_signals_model__.predict(current_data)
 
     def calculate_position_size(self, ticker: str, price: float, portfolio_value: float) -> float:
-        pass
+        return self.__calculate_position_size_model__.predict(ticker, price, portfolio_value)
+
+    def train_generate_signals(self):
+        """
+        Train __generate_signals_model__ model. The updated weights are stored.
+        """
+
+    def train_calculate_position_size(self):
+        """
+        Train __calculate_position_size_model__ model. The updated weights are stored.
+        """
--- uwqsc_algorithmic_trading/src/preprocessing/st_preprocessor_impl.py
+++ uwqsc_algorithmic_trading/src/preprocessing/st_preprocessor_impl.py
@@ -1,5 +1,6 @@
 """
-
+Before running the Simple Tensorflow Algorithm, we must prepare the data for it.
+This file contains the logic behind preprocessing data specifically for Simple Tensorflow
 """
 
 from uwqsc_algorithmic_trading.interfaces.preprocessing.preprocessor_interface import (
@@ -9,7 +10,7 @@ from uwqsc_algorithmic_trading.interfaces.preprocessing.preprocessor_interface i
 
 class STPreProcessorImpl(IPreProcessData):
     """
-    
+    Data preprocessor for the Simple Tensorflow algorithm.
     """
 
     def remove_duplicate_timestamps(self):
