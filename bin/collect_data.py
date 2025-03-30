623e9c938c631d9a35a01c04acec16ccb1d71827
paramshah07 <107650333+paramshah07@users.noreply.github.com>
2025-03-29 20:23:36 -0400

    added final touches topic/#81

--- bin/collect_data.py
+++ bin/collect_data.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
     os.makedirs(DATA_DIR, exist_ok=True)
     
     # Export tickers to CSV in the data directory
-    TICKERS_DF = pd.DataFrame(SP_TICKERS, columns=['Ticker'])
+    TICKERS_DF = pd.DataFrame(SP_TICKERS, columns=['Stock_Tickers'])
     CSV_PATH = os.path.join(DATA_DIR, 'sp_tickers.csv')
     TICKERS_DF.to_csv(CSV_PATH, index=False)
     
